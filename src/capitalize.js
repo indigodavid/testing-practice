d6d762ddc4111d9122dc024f6197e7582cff12ad
David Vera <indigodavid@gmail.com>
2022-06-07 17:27:07 -0500

    Fix linters errors

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,15 +1,14 @@
-const capitalize = string => {
+const capitalize = (string) => {
   let initIndex = 0;
   let firstChar = string.charAt(initIndex);
-  let result = '';
-  const regex = /[^A-z]/i
-  
+  const regex = /[^A-z]/i;
+
   while (regex.test(firstChar) && initIndex < string.length) {
     initIndex += 1;
     firstChar = string.charAt(initIndex);
   }
-  
-  return string.substring(0,initIndex) + firstChar.toUpperCase() + string.substring(initIndex + 1);
+
+  return string.substring(0, initIndex) + firstChar.toUpperCase() + string.substring(initIndex + 1);
 };
 
 module.exports = capitalize;
--- src/capitalize.test.js
+++ src/capitalize.test.js
@@ -8,19 +8,19 @@ const string4 = '123vera';
 describe('Capitalize first validations', () => {
   test('david returns David', () => {
     expect(capitalize(string1)).toBe('David');
-  })
-  
+  });
+
   test('vera returns Vera', () => {
     expect(capitalize(string2)).toBe('Vera');
-  })
+  });
 
   test('Castillo returns Castillo', () => {
     expect(capitalize(string3)).toBe('Castillo');
-  })
-})
+  });
+});
 
 describe('Capitalize strings beginning with numbers', () => {
   test('123vera returns 123Vera', () => {
     expect(capitalize(string4)).toBe('123Vera');
-  })
-})
+  });
+});
